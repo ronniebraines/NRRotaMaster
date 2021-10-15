89aaaf61e31dcd831edd5b89e89b125d903524fc
Ronnie <ronnie.braines@hotmail.com>
2021-10-15 09:23:03 +0100

    SDFS
    F

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
         primaryStage.setTitle("practice database");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
