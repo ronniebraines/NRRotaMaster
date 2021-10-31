cd334d66e1e0ddf043688c16af2a0926ab2e999c
ronniebraines <ronnie.braines@hotmail.com>
2021-10-31 23:57:03 +0000

    latest v, new main page and log in options and fade

---
+++ src/sample/IntroPageController.java
@@ -0,0 +1,85 @@
+package sample;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class IntroPageController implements Initializable {
+
+    public Button continueButton;
+    @FXML
+    private Circle myCircle1, myCircle, myCircle2;
+    @FXML
+    private AnchorPane rootPane;
+
+
+    @FXML
+    void handleClick(ActionEvent event) {
+        fadeOut();
+    }
+
+    private void fadeOut() {
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(rootPane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+
+        fadeTransition.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                loadNextScene("loginpage.fxml");
+            }
+        });
+        fadeTransition.play();
+    }
+
+    public void loadNextScene(String nextScene){
+        try {
+            Parent secondView;
+            secondView = (BorderPane) FXMLLoader.load(getClass().getResource(nextScene));
+            Scene nextPage = new Scene(secondView);
+
+            Stage curStage = (Stage) rootPane.getScene().getWindow();
+            curStage.setScene(nextPage);
+        }catch (IOException ex){
+            Logger.getLogger(IntroPageController.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        translateNodeUp(myCircle1,1000, -25);
+        translateNodeUp(myCircle,1100, -25);
+        translateNodeUp(myCircle2,1200, -25);
+        //giving the translate function the node, speed and distance to move
+    }
+    private void translateNodeUp (Node node, double duration, double distance){
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(node);
+        translate.setDuration(Duration.millis(duration));
+        translate.setByY(distance);
+        translate.setCycleCount(TranslateTransition.INDEFINITE);
+        translate.setAutoReverse(true);
+        translate.play();
+    }
+}
