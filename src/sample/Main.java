3c0eef0021b36ec8c245b19ef1819b5654e2b77f
ronniebraines <ronnie.braines@hotmail.com>
2021-11-16 22:14:27 +0000

    added full menu and slider animation.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -14,7 +15,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("intropage.fxml"));
         Image icon = new Image("sample/nrlogo.png");
         primaryStage.setTitle("Rota Master");
-        primaryStage.setResizable(true);
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 1015, 678));
         primaryStage.getIcons().add(icon); //setting the app icon as the network rail logo
         primaryStage.show();
--- src/sample/MainPageController.java
+++ src/sample/MainPageController.java
@@ -1,57 +1,93 @@
 package sample;
 
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
+import java.awt.desktop.SystemSleepListener;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable {
+
+
+    @FXML
+    private Label Menu;
+
+    @FXML
+    private Label MenuBack;
+
     @FXML
     private AnchorPane anchorPane;
 
     @FXML
-    private Button SELECT_BUTTON;
+    private AnchorPane menuInterface;
 
     @FXML
-    private Button VIEW_BUTTON;
+    private JFXHamburger hamOpener;
 
-    @FXML
-    private Button EDIT_BUTTON;
-
-    @FXML
-    private Button NEW_ROTA_BUTTON;
-
-    @FXML
-    void editCurrentRota(ActionEvent event) {
-
-    }
-
-
-    @FXML
-    void generateNewRota(ActionEvent event) {
-
-    }
-
-    @FXML
-    void selectCurrentWeek(ActionEvent event) {
-
-    }
-
-    @FXML
-    void viewCurrentRota(ActionEvent event) {
-
-    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         anchorPane.setOpacity(0);
         FadeInTransition();
-    }
+        menuInterface.setTranslateX(-340);
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamOpener);
+        transition.setRate(-1);
+        hamOpener.addEventHandler(MouseEvent.MOUSE_PRESSED, (event -> {
+            if(transition.getRate() == -1){
+                openSlide();
+            }else{
+                closeSlide();
+            }
+            transition.setRate(transition.getRate() * -1);
+            transition.play();
+        }));
+
+
+
+//        Menu.setOnMouseClicked(event -> {
+//            TranslateTransition slide = new TranslateTransition();
+//            slide.setDuration(Duration.seconds(0.4));
+//            slide.setNode(menuInterface);
+//
+//            slide.setToX(0);
+//            slide.play();
+//
+//            menuInterface.setTranslateX(-340);
+//
+//            slide.setOnFinished((ActionEvent e) -> {
+//                Menu.setVisible(false);
+//                MenuBack.setVisible(true);
+//            });
+//        });
+//
+//        MenuBack.setOnMouseClicked(event -> {
+//            TranslateTransition slide = new TranslateTransition();
+//            slide.setDuration(Duration.seconds(0.4));
+//            slide.setNode(menuInterface);
+//
+//            slide.setToX(-340);
+//            slide.play();
+//
+//            menuInterface.setTranslateX(0);
+//
+//            slide.setOnFinished((ActionEvent e) -> {
+//                Menu.setVisible(true);
+//                MenuBack.setVisible(false);
+//            });
+//        });
+}
+
     private void FadeInTransition() {
         FadeTransition fadeTransition = new FadeTransition();
         fadeTransition.setDuration(Duration.millis(1000));
@@ -60,4 +96,28 @@ public class MainPageController implements Initializable {
         fadeTransition.setToValue(1);
         fadeTransition.play();
     }
+
+    private void openSlide() {
+        TranslateTransition slide = new TranslateTransition();
+        slide.setDuration(Duration.seconds(0.4));
+        slide.setNode(menuInterface);
+
+        slide.setToX(0);
+        slide.play();
+
+        menuInterface.setTranslateX(-340);
+
+    }
+
+    private void closeSlide() {
+        TranslateTransition slide = new TranslateTransition();
+        slide.setDuration(Duration.seconds(0.4));
+        slide.setNode(menuInterface);
+
+        slide.setToX(-340);
+        slide.play();
+
+        menuInterface.setTranslateX(0);
+    }
+
 }
